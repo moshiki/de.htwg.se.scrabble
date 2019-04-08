13287033fb2389b3b2eb1cb6fcd0cd1a3728ef80
moshiki <felix.g.maier@web.de>
2019-04-08 19:06:10 +0200

    changed project structure added model Player.scala added main object Scrabble.scala

---
+++ src/main/scala/de/htwg/se/scrabble/Scrabble.scala
@@ -0,0 +1,11 @@
+package de.htwg.se.scrabble
+
+import de.htwg.se.scrabble.model.Player
+
+object Scrabble {
+  def main(args: Array[String]): Unit= {
+    val player = Player("Your name")
+    println("Hi " + player.name + "!")
+  }
+
+}
