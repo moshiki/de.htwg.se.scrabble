9dc04f19a7a7345d5091ebe96199887e722876a9
G-Nox <10e17@web.de>
2019-05-10 00:35:43 +0200

    Add TUI Test

--- src/test/scala/de/htwg/se/scrabble/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/scrabble/aview/TUISpec.scala
@@ -3,6 +3,6 @@ package de.htwg.se.scrabble.aview
 import org.scalatest._
 
 class TUISpec extends WordSpec with Matchers {
-  "The TUI" shut "give Text Stings back!"
+  "The TUI give Text Stings back!"
 
 }
