fe4456b8bda2f6c0d947888cb7e5ed8f58d253b0
G-Nox <10e17@web.de>
2019-05-10 12:38:45 +0200

    small Code Cleanup

--- src/test/scala/de/htwg/se/scrabble/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/scrabble/aview/TUISpec.scala
@@ -3,6 +3,14 @@ package de.htwg.se.scrabble.aview
 import org.scalatest._
 
 class TUISpec extends WordSpec with Matchers {
-  "The TUI give Text Stings back!"
+  "The TUI is unspected" when { "new" should {
+  //  val ct = new Controller
+  //  val tui = new TUI(ct)
+
+    "Gives Strings Back" in {
+      // tui.head()
+    }
+  }
+  }
 
 }
