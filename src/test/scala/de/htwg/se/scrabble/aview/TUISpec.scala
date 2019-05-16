cced241cfd333d29475a2190ec5d5ccf5545acd4
G-Nox <10e17@web.de>
2019-05-16 11:05:04 +0200

    Merge branch 'SE-07-DesignPattern' of C:\Users\E17\Desktop\HTWG\01_IDEA_Projects\IntelliJ\de.htwg.se.scrabble with conflicts.

--- src/test/scala/de/htwg/se/scrabble/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/scrabble/aview/TUISpec.scala
@@ -1,8 +1,25 @@
 package de.htwg.se.scrabble.aview
 
+import de.htwg.se.scrabble.controller.Controller
 import org.scalatest._
 
 class TUISpec extends WordSpec with Matchers {
-  "The TUI give Text Stings back!"
+  "The TUI is unspected" when { "new" should {
+  //  val ct = new Controller
+  //  val tui = new TUI(ct)
+
+  //  "Gives Strings Back" in {
+  //    tui.artScrabble() should include("--------------")
+  //  }
+
+  //  "Gives Strings Back" in {
+  //     tui.head() should include("SCRABBLE")
+  //  }
+
+  //  "Gives Strings Back" in {
+  //    tui.help() should include("commands")
+ //   }
+  }
+  }
 
 }
