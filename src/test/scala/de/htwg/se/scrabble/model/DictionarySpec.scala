ec40ec13219cee0eff585d82f9a01f4f5fc266d7
moshiki <felix.g.maier@web.de>
2019-05-24 03:45:08 +0200

    fixed test

--- src/test/scala/de/htwg/se/scrabble/model/DictionarySpec.scala
+++ src/test/scala/de/htwg/se/scrabble/model/DictionarySpec.scala
@@ -40,7 +40,7 @@ class DictionarySpec extends WordSpec with Matchers {
         /*val stream = new java.io.ByteArrayOutputStream()
         Console.withOut(stream) {dictToString()}
         // windows: stream.toString should be("Word list:\r\na\r\nb\r\nc\r\nd\r\n")
-        stream.toString*/ dictToString should be("Word list:\na\nb\nc\nd\n")
+        stream.toString*/ dictToString should be("Word list:\na\nb\nc\nd")
       }
       "print out the alphabet map in sorted order" in {
         /*val stream = new java.io.ByteArrayOutputStream()
@@ -48,7 +48,7 @@ class DictionarySpec extends WordSpec with Matchers {
           printVector()
         }
         // windows: stream.toString should be("Alphabet vector:\r\n(A,2)\r\n(B,4)\r\n(C,1)\r\n(D,3)\r\n")
-        stream.toString*/ vectorToString should be("Alphabet vector:\n(A,2)\n(B,4)\n(C,1)\n(D,3)\n")
+        stream.toString*/ vectorToString should be("Alphabet vector:\n(A,2)\n(B,4)\n(C,1)\n(D,3)")
       }
     }}
 }
