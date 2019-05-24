9bf2ead72326ff1ee2f562b5169495758e7575f2
moshiki <felix.g.maier@web.de>
2019-05-24 02:37:13 +0200

    added cell test

--- src/test/scala/de/htwg/se/scrabble/model/CellSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/model/CellSpec.scala
@@ -3,27 +3,34 @@ import org.scalatest._
 
 class CellSpec extends WordSpec with Matchers {
   "A cell" when {
+    val cell = new Cell("")
     "is empty" should {
-      val cell = new Cell("")
-      "contain nothing" in {cell.value should be("_")}
+      "contain nothing" in {cell.getValue should be("_")}
     }
     "is set" should {
       val cell = new Cell("Acv")
       "contain only one capital letter or is empty" in {
-        cell.value should fullyMatch regex "[A-Z]||[^$]"
+        cell.getValue should fullyMatch regex "[A-Z]||[^$]"
       }
+    }
+    "setValue is called set the specific value" in {
+      cell.setValue("F")
+      cell.getValue should be("F")
+    }
+    "getValue is called return value" in {
+      cell.getValue should be("F")
     }
   }
   "isLetter(str: String)" when { "str is not a capital letter" should {
       val cell = new Cell(letter = "x")
       "return false" in {
-        false === cell.isLetter(cell.value)
+        false === cell.isLetter(cell.getValue)
       }
   }
   "str is a capital letter" should {
       val cell = new Cell(letter = "A")
       "return true" in {
-        true === cell.isLetter(cell.value)
+        true === cell.isLetter(cell.getValue)
       }
     }
   }
