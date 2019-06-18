10de4449db84ca6e67dd37abf469afe56ae5cd08
moshiki <felix.g.maier@web.de>
2019-06-19 01:50:32 +0200

    added roundManager

--- src/main/scala/de/htwg/se/scrabble/model/gameManager/GameOverManager.scala
+++ src/main/scala/de/htwg/se/scrabble/model/gameManager/GameOverManager.scala
@@ -1,9 +1,16 @@
 package de.htwg.se.scrabble.model.gameManager
 
-case class GameOverManager() extends GameManager {
+import de.htwg.se.scrabble.controller.Controller
+
+case class GameOverManager(controller:Controller) extends GameManager {
   override var status: String = _
+  override var nextState: GameManager = PreSetupManager(this.controller)
+
+  switchToNextState
 
   override def getStatus: Boolean = ???
 
   override def statusToString: String = ???
+
+  override def switchToNextState: Unit = controller.roundManager = nextState
 }
--- src/main/scala/de/htwg/se/scrabble/model/gameManager/PreSetupManager.scala
+++ src/main/scala/de/htwg/se/scrabble/model/gameManager/PreSetupManager.scala
@@ -4,8 +4,13 @@ import de.htwg.se.scrabble.controller.{Controller, GameStatus}
 
 case class PreSetupManager(controller:Controller) extends GameManager {
   override var status: String = _
+  override var nextState: GameManager = SetupManager(controller)
+
+  switchToNextState
 
   override def getStatus: Boolean = ???
 
   override def statusToString: String = ???
+
+  override def switchToNextState: Unit = controller.roundManager = nextState
 }
