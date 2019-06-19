209ffac9ea01354c65cc8a67f6bc01c69cf70903
moshiki <felix.g.maier@web.de>
2019-06-19 04:11:15 +0200

    added set command syntax

--- src/main/scala/de/htwg/se/scrabble/controller/SetCommand.scala
+++ src/main/scala/de/htwg/se/scrabble/controller/SetCommand.scala
@@ -3,8 +3,8 @@ package de.htwg.se.scrabble.controller
 import de.htwg.se.scrabble.util.Command
 
 class SetCommand(row: Int, col: String, value: String, controller: Controller) extends Command {
-  override def doStep:   Unit = ???
-  override def undoStep: Unit = ???
-  override def redoStep: Unit = ???
+  override def doStep:   Unit = controller.field.setCell(row, col, value)
+  override def undoStep: Unit = controller.field.setCell(row, col, "_")
+  override def redoStep: Unit = controller.field.setCell(row, col, value)
 }
 
