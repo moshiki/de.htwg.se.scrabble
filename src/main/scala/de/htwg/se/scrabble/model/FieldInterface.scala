acd7a84af1804bc2d1cdd9cdb31985e0e68023db
moshiki <felix.g.maier@web.de>
2019-06-27 22:54:09 +0200

    changed impl of field

--- src/main/scala/de/htwg/se/scrabble/model/FieldInterface.scala
+++ src/main/scala/de/htwg/se/scrabble/model/FieldInterface.scala
@@ -8,13 +8,21 @@ trait FieldInterface {
 
   def getCell(x: String, y: Int): Option[Cell] //return cell for the coordinates
 
+  def getNextCell(cell: Cell): Option[Cell]
+
+  def getPrevCell(cell: Cell): Option[Cell]
+
+  def getUpperCell(cell: Cell): Option[Cell]
+
+  def getLowerCell(cell: Cell): Option[Cell]
+
   def setCell(x: String, y: Int, value: String): Boolean
 
   def toString: String //return field as grid
 
-  def getRows: String //return field as concatenation of rows
+  //def getRows: String //return field as concatenation of rows
 
-  def getCols: String //return filed as concatenation of columns
+  //def getCols: String //return filed as concatenation of columns
 
   def getSize: Int //returnnd the field size
 }
--- src/main/scala/de/htwg/se/scrabble/model/field/RegularField.scala
+++ src/main/scala/de/htwg/se/scrabble/model/field/RegularField.scala
@@ -3,6 +3,7 @@ package de.htwg.se.scrabble.model.field
 import de.htwg.se.scrabble.controller.GameStatus
 import de.htwg.se.scrabble.model.FieldInterface
 
+import scala.collection.mutable
 import scala.collection.mutable.SortedMap
 
 
@@ -16,36 +17,39 @@ case class RegularField(size: Int) extends FieldInterface {
     for (a <- 65 until (65 + size)) {
       var col = a.toChar.toString
       grid(row) += (col -> new Cell("_"))
-      /*//grid += (col -> SortedMap.empty[String, Cell])
-      for (col <- 0 until size) {
-        grid(row) += (col -> new Cell("_"))
-        //field += (new Coordinate(a.toChar.toString, i)-> new Cell("_"))
-      }*/
     }
   }
-
-  /*for ( i <- 0 until size){
-
-    for ( j <- 0 until size){
-      matrix(i)(j) = new Cell("_")
-    }
-  }*/
-
   setCell((65+size/2).toChar.toString, size/2, "*")
-  //matrix(size/2)(size/2) = new Cell("X")
 
-
-  override def getCell(row: String, col: Int): Option[Cell] = {
-    val X = row.toUpperCase().charAt(0)-65
-    if (X < size && col < size) {
-      Some(grid(col)(row))
+  override def getCell(col: String, row: Int): Option[Cell] = {
+    val X = col.toUpperCase().charAt(0)-65
+    if (X < size && row < size) {
+      Some(grid(row)(col))
     } else {
       controller.gameStatus = GameStatus.OOBOUND
       None
     }
   }
 
-  //override def getNextCell(cell: Cell): Option[Cell] = {None  }
+  override def getNextCell(cell: Cell): Option[Cell] = {
+    var coord = getCoordinates(cell).getOrElse(return None)
+    getCell((coord.col+1).toString, coord.row)
+  }
+
+  override def getPrevCell(cell: Cell): Option[Cell] = {
+    var coord = getCoordinates(cell).getOrElse(return None)
+    getCell((coord.col-1).toString, coord.row)
+  }
+
+  override def getUpperCell(cell: Cell): Option[Cell] = {
+    var coord = getCoordinates(cell).getOrElse(return None)
+    getCell(coord.col.toString, coord.row-1)
+  }
+
+  override def getLowerCell(cell: Cell): Option[Cell] = {
+    var coord = getCoordinates(cell).getOrElse(return None)
+    getCell(coord.col.toString, coord.row+1)
+  }
 
   override def setCell(row: String, col: Int, value: String): Boolean = {
     val c = getCell(row.toUpperCase(),col)
@@ -57,41 +61,44 @@ case class RegularField(size: Int) extends FieldInterface {
 
   override def toString: String = {
     var board: String = "|"
-    //for (a <- 65 until (65+ size)) board += a.toChar + "|"
     val r = 65 until 65+size
     r.foreach(col => board += col.toChar + "|")
     board += "\n_______________________________\n"
-    grid.foreach(row => {board += row._1
-                         row._2.foreach(col => board += "|" + col._2.getValue)
-                         board += "\n"})
-    /*for ( i <- 0 until size){
-      for ( j <- 0 until size){
-        board = board + "|" + grid(i)(j).getValue
-      }
-      board = board + "| " + i + "\n"
-    }*/
-    board = board + "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n"
+    grid.foreach(row => {row._2.foreach(col => board += "|" + col._2.getValue)
+                         board += "| " + row._1 + "\n"})
+    board += "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n"
     board
   }
 
-  override def getRows: String = {
+  private def getCoordinates(cell: Cell): Option[Coordinate] = {
+    grid.foreach(y => {
+      val revMap: mutable.Map[Cell, String] = y._2 map {_.swap}
+      if (revMap.contains(cell)) {
+        return Some(Coordinate(y._1, revMap(cell).charAt(0)))
+      }
+    })
+    None
+  }
+
+  case class Coordinate(row: Int, col: Char)
+
+  /*override def getRows: String = {
     val str = new StringBuilder
     for { i <- 0 until size
           j <- 0 until size
     } str.append(s"${matrix(i)(j).getValue}")
     str.toString()
-  }
+  }*/
 
-  override def getCols: String = {
+  /*override def getCols: String = {
     val str = new StringBuilder
     for { i <- 0 until size
           j <- 0 until size
     } str.append(s"${matrix(j)(i).getValue}")
     str.toString()
-  }
+  }*/
 
   override def getSize: Int = {
     size
   }
-  // undo je runde: spieler und deren Punktestand. Zustand des Feldes. Karten der Hand.
 }
