d7fec7188069c0a9d62346cb2290708823007c43
moshiki <felix.g.maier@web.de>
2019-07-05 03:31:51 +0200

    added tests

--- src/test/scala/de/htwg/se/scrabble/model/cards/RegularCardStackSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/model/cards/RegularCardStackSpec.scala
@@ -22,7 +22,8 @@ class RegularCardStackSpec extends WordSpec with Matchers {
         if(size > 0) {
           stack2.getSize should be < size
         }
-        stack2.putCard(Card("A"))
+        stack2.putCard(Card("C"))
+        stack2.getSpecificCard(Card("C"))
       }
 
       "return a None when getCard is invoked and stack is empty" in {
